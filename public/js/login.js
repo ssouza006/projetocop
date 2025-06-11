558cfcd27e89a0f621709a5f5583f6a9be4f7804
SANDRO GIGA <101947703+ssouza006@users.noreply.github.com>
2025-06-11 15:46:55 -0300

    Delete public directory

--- public/js/login.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('login-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const nome = document.getElementById('username').value;
-  const sala = document.getElementById('room').value;
-  sessionStorage.setItem('username', nome);
-  sessionStorage.setItem('room', sala);
-  window.location.href = '/chat.html';
-});
